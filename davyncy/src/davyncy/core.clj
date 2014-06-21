3b34c89961f01fb427835d2b4a9648434f138f47
stephane <stehem@fastmail.fm>
2014-06-21 14:28:22 +0200

    find overlap

--- davyncy/src/davyncy/core.clj
+++ davyncy/src/davyncy/core.clj
@@ -14,33 +14,68 @@
   [string]
   (rest (clojure.string/split string #"")))
 
-;ABCDE CDEFG
+(defn pad-front
+  [times coll]
+  (if (= 0 times)
+    coll
+    (last (take times (iterate #(conj % nil) (into coll [nil]))))))
 
-(defn has-character?
-  [character string]
-  (map #(first %)
-    (filter #(= character (second %))
-      (map-indexed vector (split-by-char string)))))
+(defn pad-back
+  [times coll]
+  (if (= 0 times)
+    coll
+    (reverse (pad-front times (reverse coll)))))
 
+(defn pad
+  [times-front times-back coll]
+  (pad-back times-back (pad-front times-front coll)))
 
-(defn has-substring?
-  [substring string]
-  (let [substring-split (split-by-char substring)
-        string-split (split-by-char string)
-        substring-start (first substring-split)
-        substring-start-indices (has-character? substring-start string)
-        substring-length (count substring-split)]
-    (reduce
-      (fn[memo x]  
-        (let [possible-match (take substring-length (drop x string-split))]
-          (if (or (= substring-split possible-match) (= true memo))
-            true
-            false)))
-      false
-      substring-start-indices)))
-  
+(defn padded-without-nils
+  [coll]
+  (remove #(= nil (second %))
+    (map-indexed vector coll)))
 
+(defn bounds
+  [coll]
+  (let [padded-without-nils (padded-without-nils coll)]
+  [(first (first padded-without-nils)) (first (last padded-without-nils))]))
 
+(defn interval-overlaps
+  [int1 int2]
+  (let [all (sort (into int1 int2))]
+    [(nth all 1) (nth all 2)]))
 
+(defn sub-seq
+  [itvl coll]
+  (let [takee (+ 1 (- (second itvl) (first itvl)))
+        dropee (first itvl)]
+    (take takee (drop dropee coll))))
 
+(defn slide
+  [total start-pad reference-pad]
+  (loop [start 0
+         end total
+         overlap nil]
+      (if (= -1 end)
+        overlap
+        (let [new-pad (pad start end start-pad)
+              new-pad-bounds (bounds new-pad)
+              itvl-overlaps (interval-overlaps (bounds reference-pad) new-pad-bounds)
+              sub-seq-new-pad (sub-seq itvl-overlaps new-pad)
+              sub-seq-reference (sub-seq itvl-overlaps reference-pad)
+              overlap? (= sub-seq-new-pad sub-seq-reference)]
+          (recur (inc start) (dec end)
+            (if overlap? 
+              (if (or (nil? overlap) (> (count (sub-seq-new-pad)) (count overlap)))
+                sub-seq-new-pad
+                overlap)
+              overlap))))))
 
+(defn find-overlap
+  [string1 string2]
+  (let [total (- (+ (count string1) (count string2)) 2)
+        smaller (if (>= (count string1) (count string2)) string2 string1)
+        bigger (if (= smaller string1) string2 string1)
+        start-pad (pad 0 total (split-by-char smaller))
+        reference-pad (pad (- (count smaller) 1) (- (count smaller) 1) (split-by-char bigger))]
+    (slide total start-pad reference-pad)))
